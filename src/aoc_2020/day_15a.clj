ce274d414987b5794cda35030eb177c7e0674ef0
mvermeir <mathias.vermeir@gmail.com>
2020-12-16 00:25:46 +0100

    2020 day 15 parts a & b improve turn limit

--- src/aoc_2020/day_15a.clj
+++ src/aoc_2020/day_15a.clj
@@ -7,17 +7,17 @@
 (def example-input (list 0 3 6))
 
 (defn play-game [turn last-spoken history remaining-turns]
-  (if (not= remaining-turns 0)
-    (let [[x x-1 & xs] (history last-spoken)
-          spoken-now (if x-1 (- x x-1) 0)
-          new-history (assoc history spoken-now (cons turn (history spoken-now)))]
-      (recur (inc turn) spoken-now new-history (dec remaining-turns)))
-    last-spoken))
+  (let [[x x-1 & xs] (history last-spoken)
+        spoken-now (if x-1 (- x x-1) 0)
+        new-history (assoc history spoken-now (cons turn (history spoken-now)))]
+    (if (not= remaining-turns 0)
+      (recur (inc turn) spoken-now new-history (dec remaining-turns))
+      spoken-now)))
 
 (defn solve [input]
   (let [current-turn (inc (count input))
         history-at-start (into {} (map-indexed (fn [idx x] [x (list (inc idx))]) input))]
-    (play-game current-turn 6 history-at-start (- 2021 current-turn))))
+    (play-game current-turn 6 history-at-start (- 2020 current-turn))))
 
 ;; TESTS
 (defn test-solution [] (= (solve example-input) 436))
--- src/aoc_2020/day_15b.clj
+++ src/aoc_2020/day_15b.clj
@@ -4,23 +4,18 @@
 
 (def input (list 6 19 0 5 7 13 1))
 
-(def example-input (list 0 3 6))
-
 (defn play-game [turn last-spoken history remaining-turns]
-  (if (not= remaining-turns 0)
-    (let [[x x-1 & xs] (history last-spoken)
-          spoken-now (if x-1 (- x x-1) 0)
-          new-history (assoc history spoken-now (cons turn (history spoken-now)))]
-      (recur (inc turn) spoken-now new-history (dec remaining-turns)))
-    last-spoken))
+  (let [[x x-1 & xs] (history last-spoken)
+        spoken-now (if x-1 (- x x-1) 0)
+        new-history (assoc history spoken-now (cons turn (history spoken-now)))]
+    (if (not= remaining-turns 0)
+      (recur (inc turn) spoken-now new-history (dec remaining-turns))
+      spoken-now)))
 
 (defn solve [input]
   (let [current-turn (inc (count input))
         history-at-start (into {} (map-indexed (fn [idx x] [x (list (inc idx))]) input))]
-    (play-game current-turn 6 history-at-start (- 30000001 current-turn))))
-
-;; TESTS
-(defn test-solution [] (= (solve example-input) 436))
+    (play-game current-turn 6 history-at-start (- 30000000 current-turn))))
 
 (defn -main
   "Main function"
